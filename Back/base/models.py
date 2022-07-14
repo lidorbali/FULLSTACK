9f39693ca5c8ef5d333cea8a1e4c1ab7b3337cf3
lidorbali <99362432+lidorbali@users.noreply.github.com>
2022-07-14 22:24:06 +0300

    first commit

---
+++ Back/base/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Product(models.Model):
+    desc = models.CharField(max_length=50,null=True,blank=True)
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    createdTime=models.DateTimeField(auto_now_add=True)
+    _id=models.AutoField(primary_key=True,editable=False)
+    
+    def __str__(self):
+        return self.desc
